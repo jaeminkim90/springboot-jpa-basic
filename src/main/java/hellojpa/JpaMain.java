07791b6f645b55fb713d8b019066ea37854e0d82
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-01 18:20:02 +0900

    feat: 1차로 DB에서 SQL을 이용하여 조회 후 2차 조회는 1차 캐시에서 사용하는 예제 코드 구현

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,24 +20,11 @@ public class JpaMain {
         tx.begin(); // 트랜잭션 시작
 
         try {
-
-            // 비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
             // 영속
-            System.out.println(" === BEFORE ===");
-            em.persist(member);
-            System.out.println(" === AFTER ===");
-
-            Member findMember = em.find(Member.class, 101L);
-
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
 
             tx.commit(); // 트랜잭션 저장 -> 영속성 컨텍스트에서 DB에 쿼리가 날라간다
-
         } catch (Exception e) {
             tx.rollback();
         } finally {
