d671237aa7ea212973036f6189a9aa90ea472c50
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-01 16:50:26 +0900

    feat: JPA를 이용한 업데이트 쿼리 구현

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -11,6 +11,7 @@ public class JpaMain {
         // 만드는 순간 DB 연결까지 완료 됨
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
+        // EntityManager는 자바 컬렉션처럼 객체를 대신 저장해주는 역할을 한다고 생각하면 좋다
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
@@ -18,12 +19,9 @@ public class JpaMain {
         tx.begin(); // 트랜잭션 시작
 
         try {
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("helloB");
+            Member findMember = em.find(Member.class, 1L);
 
-            em.persist(member);
-
+            findMember.setName("HelloJPA");
             tx.commit(); // 트랜잭션 저장
 
         } catch (Exception e) {
