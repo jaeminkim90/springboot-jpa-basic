ce8fec2e6c53a7e87c80e1eb54a05bb5dce9e956
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-07 15:10:48 +0900

    feat: JPA를 이용해 새로운 Member data를 저장하는 예제 코드 구현

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,20 +2,34 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
 
     public static void main(String[] args) {
-        // Factory 생성
+        // Factory 생성 -> 애플리케이션 로딩 시점에 하나만 만든다.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
-        // Factory에서 EntityManager를 꺼낸다
+        // Factory에서 EntityManager를 만든다
+        // 일괄적인 작업을 수행할 때는 Entity Manager를 꼭 만들어야 한다
         EntityManager em = emf.createEntityManager();
 
+        // JPA는 트랜잭션 범위 안에서 동작한다. 트랜잭션을 얻어야 한다.
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); // transaction 시작
 
-        // JPA 코드를 구현하는 영역
+        // Member 저장하기
 
+        // 1. 일단 member 인스턴스를 생성한다
+        Member member = new Member();
+        // 2. Id와 Name을 넣는다
+        member.setId(1L);
+        member.setName("Hello");
+
+        // 3. member를 저장한다
+        em.persist(member);
+        tx.commit(); // 트랜잭션 커밋
 
         em.close(); // 사용이 끝나면 닫아준다
         emf.close(); // 완전히 끝나면 Factory도 닫아준다
