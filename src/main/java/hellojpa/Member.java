72e5ce314ac9c4e6831b1965b8bee7435c6d3cb0
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-08 11:45:44 +0900

    feat: @SequenceGenerator를 이용해 원하는 sequenceName으로 sequence 생성

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,11 +6,12 @@ import java.util.Date;
 @Entity // 꼭 넣어야 함 -> JPA가 관리하는 객체로 인식한다. DB 테이블과 맵핑하여 사용한다.
 // @Table -> 일반적으로 Table 이름은 Member를 따라가지면 @Table을 이용하여 별도로 지정할 수도 있다
 // ex) @Table(name = "MBR")
+@SequenceGenerator(name = "member-seq_generator", sequenceName = "member_seq")
 public class Member {
 
     @Id // PK 매핑
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member-seq_generator")
+    private Long id;
 
     @Column(name = "name", nullable = false) // 객체와 DB의 Colum이 다를 경우
     private String username;
@@ -18,11 +19,11 @@ public class Member {
     public Member() {
     }
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
