53e13817fb42b9aa5ff70397fc9b3b6f0d940d38
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-08 10:44:11 +0900

    feat: 회원 요구사항에 맞춰 Member 클래스 수정 및 RoleType Enum 추가

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,40 +1,32 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
-@Entity // 꼭 넣어야 함 -> JPA를 사용하는 객체로 인식한다
+@Entity // 꼭 넣어야 함 -> JPA가 관리하는 객체로 인식한다. DB 테이블과 맵핑하여 사용한다.
 // @Table -> 일반적으로 Table 이름은 Member를 따라가지면 @Table을 이용하여 별도로 지정할 수도 있다
+// ex) @Table(name = "MBR")
 public class Member {
 
-    @Id // JPA에 PK가 뭔지 알려준다
+    @Id
     private Long id;
 
-    // @Column -> column도 마찬가지로 별도의 이름이 있다면 name 속성을 이용하여 mapping 할 수 있다
-    private String name;
+    @Column(name = "name")
+    private String username;
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    private Integer age;
 
-    public Member() {
-    }
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
 
-    public Long getId() {
-        return id;
-    }
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    @Lob
+    private String description;
 }
+//Getter, Setter...
+
