1be5d1c381e72a36eff5b1b147be8db5ec3b3c3c
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-08 17:41:29 +0900

    feat: @TableGenerator를 이용하여 id 테이블 생성

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,12 +5,16 @@ import java.util.Date;
 
 @Entity // 꼭 넣어야 함 -> JPA가 관리하는 객체로 인식한다. DB 테이블과 맵핑하여 사용한다.
 // @Table -> 일반적으로 Table 이름은 Member를 따라가지면 @Table을 이용하여 별도로 지정할 수도 있다
-// ex) @Table(name = "MBR")
-@SequenceGenerator(name = "member-seq_generator", sequenceName = "member_seq")
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
+
 public class Member {
 
-    @Id // PK 매핑
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member-seq_generator")
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     @Column(name = "name", nullable = false) // 객체와 DB의 Colum이 다를 경우
