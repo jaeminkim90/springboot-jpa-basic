f885b3f7e5825c4b521de59355274f493d52fee5
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-10 11:42:39 +0900

    feat: @Inheritance 전략을 JOINED로 설정하면 테이블이 상속 관계 처럼 매핑된다

--- src/main/java/hellojpa/Movie.java
+++ src/main/java/hellojpa/Movie.java
@@ -1,8 +1,11 @@
 package hellojpa;
 
 import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Movie extends Item{
 
     private String director;
